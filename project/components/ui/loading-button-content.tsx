74c2d841bd664b5a77e80473ba9f47387d417e3f
Jhack Concha <i_jhack.concha@stratpoint.com>
2025-08-26 17:41:59 +0800

    fix: fix LoadingButtonContent not abiding by passed loaderSize
    Signed-off-by: Jhack Concha <i_jhack.concha@stratpoint.com>

--- project/components/ui/loading-button-content.tsx
+++ project/components/ui/loading-button-content.tsx
@@ -3,7 +3,7 @@ import { Loader2Icon } from "lucide-react";
 interface LoadingButtonContentProps {
   isLoading: boolean;
   displayText: string;
-  loaderSize?: string;
+  loaderSize?: number;
 }
 
 export function LoadingButtonContent({ isLoading, displayText, loaderSize }: LoadingButtonContentProps) {
@@ -11,7 +11,7 @@ export function LoadingButtonContent({ isLoading, displayText, loaderSize }: Loa
     <>
       {isLoading ? (
         <div className="flex gap-2 items-center">
-          <Loader2Icon className={`animate-spin ${loaderSize ? `h-${loaderSize} w-${loaderSize}` : ""} `} /> Loading
+          <Loader2Icon className={`animate-spin`} style={{width: loaderSize, height: loaderSize}} /> Loading
         </div>
       ) : (
         displayText
