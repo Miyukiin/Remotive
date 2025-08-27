752b3613e5b23a9ef184b4027167f13aad9f531c
Jhack Concha <i_jhack.concha@stratpoint.com>
2025-08-27 11:04:19 +0800

    feat: add project deletion section in project settings page
    - Refactor existing placeholder to own component.
    - Redirect user to projects page on deletion.
    
    Signed-off-by: Jhack Concha <i_jhack.concha@stratpoint.com>

--- project/app/(dashboard)/projects/[id]/settings/page.tsx
+++ project/app/(dashboard)/projects/[id]/settings/page.tsx
@@ -2,9 +2,8 @@
 import { use } from "react";
 import { useProjects } from "@/hooks/use-projects";
 import LoadingUI from "@/components/ui/loading-ui";
-import { Separator } from "@/components/ui/separator";
-import { Button } from "@/components/ui/button";
 import { ProjectGeneralSettings } from "@/components/projects/project-general-settings";
+import { ProjectDangerSettings } from "@/components/projects/project-danger-settings";
 
 export default function ProjectSettingsPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = use(params); // Unwrap the promise as per Nextjs 15 recommendation
@@ -24,34 +23,8 @@ export default function ProjectSettingsPage({ params }: { params: Promise<{ id: 
       <ProjectGeneralSettings project_id={projectId} />
 
       {/* Danger Settings */}
-      <div className="flex flex-col gap-2">
-        <p className="text-xl">Danger Zone</p>
-        <div className="p-4 border-[1px] border-destructive/50 rounded-md">
-          <div className="flex items-center gap-3 text-foreground text-sm">
-            <div>
-              <p className="font-medium"> Delete Project </p>
-              <p className="font-light"> This project will be permanently deleted.</p>
-            </div>
-
-            <Button
-              onClick={() => {}}
-              // disabled={isTeamDeleteLoading}
-              variant="destructive"
-              className="text-xs h-max"
-            >
-              {/* {isTeamDeleteLoading ? (
-                <div className="flex items-center gap-2">
-                  <Loader2Icon className="h-4 w-4 animate-spin" />
-                  Loading...
-                </div>
-              ) : (
-                "Delete team"
-              )} */}
-              Delete Project
-            </Button>
-          </div>
-        </div>
-      </div>
+      <ProjectDangerSettings project_id={projectId}/>
+      
     </div>
   );
 }
---
+++ project/components/projects/project-danger-settings.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useProjects } from "@/hooks/use-projects";
+import { Button } from "../ui/button";
+
+import { LoadingButtonContent } from "../ui/loading-button-content";
+import { redirect } from "next/navigation";
+
+type ProjectDangerSettingsProps = {
+  project_id: number;
+};
+
+export function ProjectDangerSettings({ project_id }: ProjectDangerSettingsProps) {
+  const { deleteProject, isProjectDeleteLoading } = useProjects(project_id);
+
+  function onClick() {
+    deleteProject(project_id);
+    redirect("/projects")
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+      <p className="text-xl">Danger Zone</p>
+      <div className="p-4 border-[1px] border-destructive/50 rounded-md">
+        <div className="flex items-center gap-3 text-foreground text-sm">
+          <div>
+            <p className="font-medium"> Delete Project </p>
+            <p className="font-light"> This project will be permanently deleted.</p>
+          </div>
+
+          <Button onClick={onClick} disabled={isProjectDeleteLoading} variant="destructive" className="text-xs h-max">
+            <LoadingButtonContent isLoading={isProjectDeleteLoading} displayText="Delete Project" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
