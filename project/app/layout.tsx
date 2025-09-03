1a8f38e396821a55f9eb847cf67753ef26fb5c4e
Jhack Concha <i_jhack.concha@stratpoint.com>
2025-09-03 09:13:36 +0800

    feat: overhauls settings page and reset deps
    - Added userProfile content to settings page with placeholder for
      appearance
    - Reset dependencies so clerk themes would work and be updated to the
      latest version
    
    Signed-off-by: Jhack Concha <i_jhack.concha@stratpoint.com>

--- project/app/layout.tsx
+++ project/app/layout.tsx
@@ -1,33 +1,32 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import { ClerkProvider } from "@clerk/nextjs"
-import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "sonner"
-import Providers from "@/components/providers"
-import "./globals.css";                                   
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "sonner";
+import Providers from "@/components/providers";
+import "./globals.css";
+import { shadcn } from "@clerk/themes";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Remotive | A Project Management Tool",
   description: "Remotive is the premier team collaboration and project management platform",
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-    <html lang="en" /*className="dark"*/ suppressHydrationWarning>
-      <body className={inter.className}>
-        <Toaster richColors position="top-right"/>
-        <ThemeProvider><Providers>{children}</Providers></ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider appearance={{ theme: shadcn }}>
+      <html lang="en" /*className="dark"*/ suppressHydrationWarning>
+        <body className={inter.className}>
+          <Toaster richColors position="top-right" />
+          <ThemeProvider>
+            <Providers>{children}</Providers>
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
